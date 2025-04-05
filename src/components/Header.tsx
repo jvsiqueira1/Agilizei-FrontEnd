6360a987475a14d3c0d8ebd9ef0d6f0bb5223b22
João Vítor de Siqueira Campos <joaovitorsc@gmail.com>
2025-04-05 12:15:59 -0400

    Fix: Centralizei a Header e coloquei um padding no Footer

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -20,29 +20,31 @@ export default function Header() {
 
   return (
     <header className="w-screen bg-light-gray flex justify-between p-8 z-50 border-b-2">
-      <nav className="flex justify-between items-center w-[92%] mx-auto">
+      <nav className="relative flex justify-between items-center w-[92%] mx-auto">
         <div>
           <NavLink to="/">
             <img className="w-32" src="agilizeiLogo.svg" alt="Agilizei Logo" />
           </NavLink>
         </div>
-        <ul className="hidden md:flex items-center gap-[4vw]">
-          <li>
-            <NavLink to="/about" className="hover:text-orange">
-              Sobre
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/services" className="hover:text-orange">
-              Serviços
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/FAQ" className="hover:text-orange">
-              FAQ
-            </NavLink>
-          </li>
-        </ul>
+        <div className="hidden md:flex absolute left-[50%] -translate-x-[54%]">
+          <ul className="flex flex-row items-center gap-[4vw]">
+            <li>
+              <NavLink to="/about" className="hover:text-orange">
+                Sobre
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/services" className="hover:text-orange">
+                Serviços
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/FAQ" className="hover:text-orange">
+                FAQ
+              </NavLink>
+            </li>
+          </ul>
+        </div>
         <div className="hidden md:flex gap-4">
           <Button onClick={() => setClientModal(true)} variant={'outline'}>
             Área do Cliente
